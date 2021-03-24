531456076ba7bdac931378dc0e322263da17a42f
sachicorrea <scorrear@tutanota.com>
2021-03-24 14:17:02 -0700

    Index and Create views changes. HomeController parameterized sql objects

--- AppAlumni/Controllers/HomeController.cs
+++ AppAlumni/Controllers/HomeController.cs
@@ -21,117 +21,207 @@ namespace AppAlumni.Controllers
         public IActionResult Index()
         {
             List<UsersAlumini> usersAluminiList = new List<UsersAlumini>();
-
-            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
             
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                DataTable dataTable = new DataTable();
+                connection.Open();
 
                 string sql = "Select * From AlumniDb";
                 SqlCommand command = new SqlCommand(sql, connection);
-                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
 
-                dataAdapter.Fill(dataTable);
-
-                foreach (DataRow dr in dataTable.Rows)
+                using (SqlDataReader dataReader = command.ExecuteReader())
                 {
-                    UsersAlumini usersalumini = new UsersAlumini();
-                        usersalumini.Usr_id = Convert.ToInt32(dr["Usr_id"]);
-                        usersalumini.Usr_doc = Convert.ToString(dr["Usr_doc"]);
-                        usersalumini.Usr_kind_doc = Convert.ToString(dr["Usr_kind_doc"]);
-                        usersalumini.Usr_phonenum = Convert.ToString(dr["Usr_phonenum"]);
-                        usersalumini.Usr_email = Convert.ToString(dr["Usr_email"]);
-                        usersalumini.Usr_gender = Convert.ToString(dr["Usr_gender"]);
-                        usersalumini.Usr_category = Convert.ToString(dr["Usr_category"]);
-                        usersalumini.Usr_program = Convert.ToString(dr["Usr_program"]);
-                        usersalumini.Usr_startdate = Convert.ToDateTime(dr["Usr_startdate"]);
-                        usersalumini.Usr_address = Convert.ToString(dr["Usr_address"]);
-                        usersalumini.Usr_neighborhood = Convert.ToString(dr["Usr_neighborhood"]);
-                        usersalumini.Usr_city = Convert.ToString(dr["Usr_city"]);
-                        usersalumini.Usr_province = Convert.ToString(dr["Usr_province"]);
-                        usersalumini.Usr_dateregistr = Convert.ToDateTime(dr["Usr_dateregistr"]);
+                    while (dataReader.Read())
+                    {
+                        UsersAlumini usersalumini = new UsersAlumini();
+                        usersalumini.Usr_id = Convert.ToInt32(dataReader["Usr_id"]);
+                        usersalumini.Usr_doc = Convert.ToString(dataReader["Usr_doc"]);
+                        usersalumini.Usr_kind_doc = Convert.ToString(dataReader["Usr_kind_doc"]);
+                        usersalumini.Usr_phonenum = Convert.ToString(dataReader["Usr_phonenum"]);
+                        usersalumini.Usr_email = Convert.ToString(dataReader["Usr_email"]);
+                        usersalumini.Usr_gender = Convert.ToString(dataReader["Usr_gender"]);
+                        usersalumini.Usr_category = Convert.ToString(dataReader["Usr_category"]);
+                        usersalumini.Usr_program = Convert.ToString(dataReader["Usr_program"]);
+                        usersalumini.Usr_startdate = Convert.ToDateTime(dataReader["Usr_startdate"]);
+                        usersalumini.Usr_address = Convert.ToString(dataReader["Usr_address"]);
+                        usersalumini.Usr_neighborhood = Convert.ToString(dataReader["Usr_neighborhood"]);
+                        usersalumini.Usr_city = Convert.ToString(dataReader["Usr_city"]);
+                        usersalumini.Usr_province = Convert.ToString(dataReader["Usr_province"]);
+                        usersalumini.Usr_dateregistr = Convert.ToDateTime(dataReader["Usr_dateregistr"]);
 
                         usersAluminiList.Add(usersalumini);
+                    }
                 }
 
                 connection.Close();
             }
             return View(usersAluminiList);
         }
-    }
-    
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult Create(UsersAlumini usersalumini)
-    {
-        string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        /*
+        public IActionResult Index()
         {
-            string sql = "Insert Into Inventory (Usr_doc, Usr_kind_doc, Usr_name, Usr_phonenum, Usr_email, Usr_gender, Usr_category, Usr_program, Usr_startdate, Usr_address, Usr_neighborhood, Usr_city, Usr_province, Usr_dateregistr) Values (@Usr_doc, @Usr_kind_doc, @Usr_name, @Usr_phonenum, @Usr_email, @Usr_gender, @Usr_category, @Usr_program, @Usr_startdate, @Usr_address, @Usr_neighborhood, @Usr_city, @Usr_province, @Usr_dateregistr)";
-
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.CommandType = CommandType.Text;
-
-                // adding parameters
-
-                SqlParameter parameter = new SqlParameter
-                {
-                    ParameterName = "@Usr_id",
-                    Value = usersalumini.Usr_id,
-                    SqlDbType = SqlDbType.Int,
-                    Size = 50
-                };
-                command.Parameters.Add(parameter);
-
-                parameter = new SqlParameter
-                {
-                    ParameterName = "@Usr_doc",
-                    Value = usersalumini.Usr_doc,
-                    SqlDbType = SqlDbType.VarChar,
-                    Size = 50
-                };
-                command.Parameters.Add(parameter);
-
-                parameter = new SqlParameter
-                {
-                    ParameterName = "@Usr_kind_doc",
-                    Value = usersalumini.Usr_kind_doc,
-                    SqlDbType = SqlDbType.VarChar,
-                };
-                command.Parameters.Add(parameter);
-
-                parameter = new SqlParameter
-                {
-                    ParameterName = "@Result",
-                    SqlDbType = SqlDbType.VarChar,
-                    Size = 50,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(parameter);
-
-                connection.Open();
-
-                // Execute the stored procedure
-                command.ExecuteNonQuery();
-
-                // Output parameter value
-                string result = Convert.ToString(command.Parameters["@Result"].Value);
-                ViewBag.Result = result;
-
-                connection.Close();
-            }
+            return View();
         }
-        return View();
+        */
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UsersAlumini usersalumini)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "Insert Into AlumniDb (Usr_doc, Usr_kind_doc, Usr_name, Usr_phonenum, Usr_email, Usr_gender, Usr_category, Usr_program, Usr_startdate, Usr_address, Usr_neighborhood, Usr_city, Usr_province, Usr_dateregistr) Values (@Usr_doc, @Usr_kind_doc, @Usr_name, @Usr_phonenum, @Usr_email, @Usr_gender, @Usr_category, @Usr_program, @Usr_startdate, @Usr_address, @Usr_neighborhood, @Usr_city, @Usr_province, @Usr_dateregistr)";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+
+                    // adding parameters
+                    /*
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_id",
+                        Value = usersalumini.Usr_id,
+                        SqlDbType = SqlDbType.Int
+                    };
+                    command.Parameters.Add(parameter);
+                    */
+
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_doc",
+                        Value = usersalumini.Usr_doc,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 15
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_kind_doc",
+                        Value = usersalumini.Usr_kind_doc,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 15
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_name",
+                        Value = usersalumini.Usr_name,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 15
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_phonenum",
+                        Value = usersalumini.Usr_phonenum,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 10
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_email",
+                        Value = usersalumini.Usr_email,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_gender",
+                        Value = usersalumini.Usr_gender,
+                        SqlDbType = SqlDbType.Char,
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_category",
+                        Value = usersalumini.Usr_category,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 50
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_program",
+                        Value = usersalumini.Usr_program,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_startdate",
+                        Value = usersalumini.Usr_startdate,
+                        SqlDbType = SqlDbType.Date,
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_address",
+                        Value = usersalumini.Usr_address,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_neighborhood",
+                        Value = usersalumini.Usr_neighborhood,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 50
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_city",
+                        Value = usersalumini.Usr_city,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_province",
+                        Value = usersalumini.Usr_province,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Usr_dateregistr",
+                        Value = usersalumini.Usr_dateregistr,
+                        SqlDbType = SqlDbType.Date,
+                    };
+                    command.Parameters.Add(parameter);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            ViewBag.Result = "Success";
+            return View();
+        }
     }
 }
